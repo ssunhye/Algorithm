8d0a6a4310f6dcf4562314e04ef09e84f018892b
ssunhye <wkadnsj@gmail.com>
2021-07-12 22:58:04 +0900

    BOJ1927

---
+++ src/BOJ/Q1927.java
@@ -0,0 +1,38 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Q1927 {
+
+    static int N;
+    static PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = stoi(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            int x = stoi(br.readLine());
+
+            if (x==0) {
+                if (queue.isEmpty()) sb.append(0).append("\n");
+                else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else {
+                queue.offer(x);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
