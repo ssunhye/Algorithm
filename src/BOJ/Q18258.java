87718b3e62a2693fe6e14b4df3c1755375d917c7
ssunhye <wkadnsj@gmail.com>
2021-07-13 17:44:07 +0900

    BOJ18258

---
+++ src/BOJ/Q18258.java
@@ -0,0 +1,63 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Q18258 {
+
+    static int N;
+    static LinkedList<Integer> queue = new LinkedList<>();
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = stoi(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String order = st.nextToken();
+            switch (order) {
+                case "push":
+                    queue.offer(stoi(st.nextToken()));
+                    break;
+                case "front":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.peek()).append("\n");
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.peekLast()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) sb.append(1).append("\n");
+                    else sb.append(0).append("\n");
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.poll()).append("\n");
+                    }
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
