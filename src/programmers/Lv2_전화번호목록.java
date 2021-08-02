540a29e3da14e4d4cd9162050bcba734a17ca37a
ssunhye <wkadnsj@gmail.com>
2021-08-03 00:48:19 +0900

    add: programmers Lv2.전화번호목록

---
+++ src/programmers/Lv2_전화번호목록.java
@@ -0,0 +1,34 @@
+package programmers;
+
+import java.util.HashMap;
+
+public class Lv2_전화번호목록 {
+
+    public static void main(String[] args) {
+        String[] phone_book = {"119", "97674223", "1195524421"};
+
+        boolean answer = solution(phone_book);
+
+        System.out.println(answer);
+    }
+
+    public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (int i=0; i<phone_book.length; i++) {
+            hm.put(phone_book[i], i);
+        }
+
+        for (int i=0; i<phone_book.length; i++) {
+            for (int j=0; j<phone_book[i].length(); j++) {
+                if (hm.containsKey(phone_book[i].substring(0, j))) {
+                    answer = false;
+                    return answer;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
