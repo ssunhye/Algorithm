b5c5a6a36f07a5279f346fba234d54a9c1b91ec3
ssunhye <wkadnsj@gmail.com>
2021-08-03 00:06:07 +0900

    add: programmers Lv1.체육복

--- src/programmers/Lv1_체육복.java
+++ src/programmers/Lv1_체육복.java
@@ -2,14 +2,13 @@ package programmers;
 
 import java.util.Arrays;
 
-public class Q1_체육복 {
+public class Lv1_체육복 {
     public static void main(String[] args) {
         int n = 5;
         int[] lost = {2, 4};
         int[] reserve = {1, 3, 5};
 
         int answer = n-lost.length;
-
 
         return;
     }
