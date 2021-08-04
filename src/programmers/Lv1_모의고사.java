2fb8db5e59f43b28866b6cd4785ef2be786e59fc
ssunhye <wkadnsj@gmail.com>
2021-08-04 12:49:18 +0900

    add: programmers Lv1 모의고사

--- src/programmers/Lv1_모의고사.java
+++ src/programmers/Lv1_모의고사.java
@@ -8,6 +8,11 @@ public class Lv1_모의고사 {
     public static void main(String[] args) {
         int[] answers = {1,2,3,4,5};
 
+        int[] answer = solution(answers);
+        System.out.println(answer);
+    }
+
+    public static int[] solution(int[] answers) {
         int[] p1 = {1,2,3,4,5};
         int[] p2 = {2,1,2,3,2,4,2,5};
         int[] p3 = {3,3,1,1,2,2,4,4,5,5};
@@ -19,22 +24,22 @@ public class Lv1_모의고사 {
             if (p2[i%8] == answers[i]) score[1]++;
             if (p3[i%10] == answers[i]) score[2]++;
         }
-        int[] sortScore = score.clone();
-        Arrays.sort(sortScore);
+
+        int max = Math.max(Math.max(score[0], score[1]), score[2]);
 
         List<Integer> list = new ArrayList();
 
         for (int i=0; i<score.length; i++) {
-            if (score[i] == sortScore[2]) {
+            if (score[i] == max) {
                 list.add(i+1);
             }
         }
 
         int[] answer = new int[list.size()];
-        for (int i=0; i<list.size(); i++) {
+        for (int i=0; i<answer.length; i++) {
             answer[i] = list.get(i);
         }
-        System.out.println(answer[0]);
-        return;
+
+        return answer;
     }
 }
