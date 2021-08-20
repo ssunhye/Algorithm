d58e91c53ef649cf67eb7994126661f51463eba8
김선혜 <wkadnsj@gmail.com>
2021-08-20 21:52:04 +0900

    add: 프로그래머스 lv3 정수삼각형

---
+++ src/Programmers/Lv3_정수삼각형.java
@@ -0,0 +1,26 @@
+package Programmers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+
+public class Lv3_정수삼각형 {
+    public static void main(String[] args) {
+        int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
+        int answer = solution(triangle);
+        System.out.println(answer);
+    }
+
+    public static int solution(int[][] triangle) {
+        int len = triangle.length;
+        int[][] dp = new int[len+1][len+1];
+        dp[1][1] = triangle[0][0];
+        for (int i=2; i<=len; i++) {
+            for (int j=1; j<=i; j++) {
+                dp[i][j] = triangle[i-1][j-1] + Math.max(dp[i-1][j-1], dp[i-1][j]);
+            }
+        }
+        return Arrays.stream(dp[len]).max().getAsInt();
+    }
+}
