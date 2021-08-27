4ed1731ae4b1639e267a1e683e20e6e3ecd1371c
김선혜 <wkadnsj@gmail.com>
2021-08-27 17:56:30 +0900

    add: 프로그래머스 이중우선순위큐

---
+++ src/programmers/Lv3_단속카메라.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class Lv3_단속카메라 {
+    public static void main(String[] args) {
+        int[][] routes = {{-20,15}, {-14,-5}, {-18,-13}, {-5,-3}};
+
+        System.out.println(solution(routes));
+    }
+
+    public static int solution(int[][] routes) {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
---
+++ src/programmers/Lv3_이중우선순위큐.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.PriorityQueue;
+
+public class Lv3_이중우선순위큐 {
+    public static void main(String[] args) {
+        String[] operations = {"I 7","I 5","I -5","D -1"};
+
+        System.out.println(solution(operations));
+    }
+
+    public static int[] solution(String[] operations) {
+        int[] answer = {};
+
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+
+        return answer;
+    }
+}
