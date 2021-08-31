e94faf2e700c5d87c0dbc6b7c0a4f98000fbecbc
김선혜 <wkadnsj@gmail.com>
2021-08-31 20:19:00 +0900

    add: 프로그래머스 lv3 dfs 여행경로

---
+++ src/programmers/lv3_여행경로.java
@@ -0,0 +1,42 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class lv3_여행경로 {
+    public static boolean[] visited;
+    public static int n;
+    public static ArrayList<String> answer = new ArrayList<>();
+
+    public static void main(String[] args) {
+        String[][] tickets = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
+
+        System.out.println(Arrays.toString(solution(tickets)));
+    }
+
+    public static String[] solution(String[][] tickets) {
+        n = tickets.length;
+        visited = new boolean[n];
+
+        dfs("ICN", "ICN", 0, tickets);
+        Collections.sort(answer);
+
+        return  answer.get(0).split(" ");
+    }
+
+    public static void dfs(String city, String routes, int idx, String[][] tickets) {
+        if (idx == n) {
+            answer.add(routes);
+            return;
+        }
+
+        for (int i=0; i<n; i++) {
+            if (visited[i] == false && tickets[i][0].equals(city)) {
+                visited[i] = true;
+                dfs(tickets[i][1], routes+" "+tickets[i][1], idx+1, tickets);
+                visited[i] = false;
+            }
+        }
+    }
+}
