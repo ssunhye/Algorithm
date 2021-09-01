06d540d8c823f3756d4ca295c9a06b755b2ace50
김선혜 <wkadnsj@gmail.com>
2021-09-01 22:46:38 +0900

    fix: 프로그래머스 lv1 완주하지 못한 선수 다시 풀기

--- src/programmers/Lv1_완주하지못한선수.java
+++ src/programmers/Lv1_완주하지못한선수.java
@@ -1,6 +1,7 @@
 package programmers;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Lv1_완주하지못한선수 {
     public static void main(String[] args) {
@@ -13,6 +14,7 @@ public class Lv1_완주하지못한선수 {
     }
 
     public static String solution(String[] participant, String[] completion) {
+        /* 첫번째 풀이
         String answer = "";
 
         Arrays.sort(participant);
@@ -25,6 +27,43 @@ public class Lv1_완주하지못한선수 {
             }
         }
         answer = participant[participant.length - 1];
+        return answer;
+        */
+
+        /* 두번째 풀이
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i=0; i<completion.length; i++) {
+            if (participant[i].equals(completion[i])) {
+                continue;
+            } else {
+                return participant[i];
+            }
+        }
+
+        return participant[participant.length-1];
+        */
+
+        // 세번째 풀이
+        String answer = "";
+
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String p : participant) {
+            hm.put(p, hm.getOrDefault(p, 0)+1);
+        }
+
+        for (String c : completion) {
+            hm.put(c, hm.get(c) - 1);
+        }
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0) {
+                answer = key;
+                break;
+            }
+        }
+
         return answer;
     }
 }
